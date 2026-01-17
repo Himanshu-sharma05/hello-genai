3aeb4cb195c2f497ae509723763a7d5174fdcfac
Himanshu <himanshusharma052008@gmail.com>
2026-01-17 19:11:06 +0530

    gemini using openai sdk

---
+++ hello_world/gemini_openai.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[{
+        "role":"user",
+        "content":"Hey I am Himanshu sharma nice to meet you!"
+    }]
+)
+print(response.choices[0].message.content)
