a2d501aac6de9160809bc18ccc014d2a19104a32
Himanshu <himanshusharma052008@gmail.com>
2026-01-27 18:44:18 +0530

    speech to text ai response

--- voice_agent/main.py
+++ voice_agent/main.py
@@ -1,4 +1,15 @@
 import speech_recognition as sr
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
 
 def main():
     r = sr.Recognizer()
@@ -7,12 +18,32 @@ def main():
         r.adjust_for_ambient_noise(source)
         r.pause_threshold = 2
 
-        print("Speak something...")
-        audio = r.listen(source)
+        SYSTEM_PROMPT = """
+            You are an expert voice agent and you generate the text based on 
+            the transcript of what user has said 
+            you need to output as you are a voice agent and whatever you speak will be 
+            converted back to audio.
+"""
+        message_history = [{"role":"system","content":SYSTEM_PROMPT}]
+        while True:
 
-        print("Processing audio......")
-        stt = r.recognize_google(audio)
+            print("Speak something...")
+            audio = r.listen(source)
 
-        print("You said:",stt)
+            print("Processing audio......")
+            stt = r.recognize_google(audio)
+
+            print("You said:",stt)
+
+            message_history.append({"role":"user","content":stt})
+            response = client.chat.completions.create(
+                model="gemini-2.5-flash",
+                messages=message_history
+            )
+
+            result = response.choices[0].message.content
+            print(f"AI:{result}")
+
+            message_history.append({"role":"assistant","content":result})
 
 main()
